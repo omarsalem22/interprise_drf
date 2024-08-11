b529d95c521156aa2a88e28d0c5d204bfb6836c9
omarsalem22 <omarmohamed25112001@gmail.com>
2024-08-11 23:13:45 +0300

    list create api

--- backend/products/views.py
+++ backend/products/views.py
@@ -7,3 +7,28 @@ class ProductDetailApiView(generics.RetrieveAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
     lookup_field='pk'
+
+
+class ProductCreateApoView(generics.CreateAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+
+    def perform_create(self,serializer):
+        title=serializer.validated_data.get('title')
+        content=serializer.validated_data.get('content') or None
+        if content is None:
+            content=title
+        serializer.save(content=content)
+
+class ProductListCreateApiView(generics.ListCreateAPIView):
+
+       queryset=Product.objects.all()
+       serializer_class=ProductSerializer
+
+       def perform_create(self,serializer):
+            title=serializer.validated_data.get('title')
+            content=serializer.validated_data.get('content') or None
+            if content is None:
+                content=title
+            serializer.save(content=content)
+
