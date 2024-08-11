59374a097dd15ed6526828c68d9647870837eb72
omarsalem22 <omarmohamed25112001@gmail.com>
2024-08-11 22:39:16 +0300

    RetrieveAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductDetailApiView(generics.RetrieveAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    lookup_field='pk'
